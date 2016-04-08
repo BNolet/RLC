6093db3c7539039a13249f845dda0a588a02239f
Andreas Stjerneklar Kristensen <andreas.kristensen@gmail.com>
2016-04-08 23:43:02 +0200

    Update rlcs.user.js
    design update

--- rlcs.user.js
+++ rlcs.user.js
@@ -16,23 +16,78 @@ $(document).ready(function() {
   });   
  
  /*add css styles, every line must end with \  */
-     GM_addStyle(" \
-          a.author {float:left;} \
-          aside.sidebar #discussions li {background:#404040;}\
-          .md a {color:#5ED7FF!important;}\
-          .sidebar a {color:#5ED7FF!important;}\
-          body.loggedin.liveupdate-app {background:#404040;color:white;}\
-          div.content{background:#404040;color:white;}\
-          div.md{color:white;}\
-          div#liveupdate-options {color:white;}\
-          aside.sidebar {background:#404040;!important}\
-          h2{color:white!important}\
-         .liveupdate-listing li.liveupdate ul.buttonrow { display: none!important;}\
-         .liveupdate-listing li.liveupdate .time {width:80px}  \
-         .liveupdate .body {   \
-            padding: 5px; \
-            max-width:600px!important; \
-         } \
+      GM_addStyle(" \
+        .liveupdate-listing li.liveupdate ul.buttonrow { display: none!important;} \
+        .liveupdate-listing li.liveupdate .time {}   \
+        .liveupdate .body {max-width: none!important;}  \
+        aside.sidebar #discussions li {background:#404040;} \
+        .md a {color:#5ED7FF!important;} \
+        .sidebar a {color:#5ED7FF!important;} \
+        body.loggedin.liveupdate-app {background:#404040;color:white;} \
+        div.content{background:#404040;color:white;} \
+        div.md{color:white;} \
+        div#liveupdate-options {color:white;position: absolute;top: 19px;right: 18px;} \
+        aside.sidebar {background:#404040;!important} \
+        h2{color:white!important} \
+        #new-update-form .usertext {  max-width: none;} \
+.usertext-edit .md {min-width: 100%!important;} \
+.content { \
+    width: 70%; \
+    position:relative; \
+} \
+header#liveupdate-header { \
+    position: absolute; \
+    right: 10px; \
+    top:10px; \
+    width: 305px; \
+} \
+div#new-update-form { \
+    position: absolute; \
+    bottom: 5px; \
+    background: #161616; \
+    width: 74%; \
+    margin: 0; \
+    left: 10px; \
+} \
+.main-content { \
+    margin-top: 0; \
+    width: 100%; \
+} \
+aside.sidebar.side.md-container { \
+    position: absolute; \
+    right: 0; \
+    top: 190px; \
+    padding:10px; \
+} \
+ol.liveupdate-listing { \
+    max-width: 76%; \
+    overflow-y: scroll; \
+    padding-right: 15px; \
+    height:700px!important; \
+    margin-bottom: 150px; \
+} \
+.liveupdate-listing li.liveupdate {  border-top:1px solid grey;padding-top:2px; } \
+.liveupdate-listing li.liveupdate .body div.md { \
+    width: 90%; \
+    display: block; \
+    float: right; \
+    margin-bottom:0; \
+} \
+a.author { \
+    display: block; \
+    width: 15%; \
+    float: left; \
+    } \
+.liveupdate-listing li.liveupdate time { \
+    padding: 0; \
+    width: 15%; \
+    float:left; \
+} \
+.usertext-edit.md-container { \
+    max-width: 100%; \
+    margin: 0; \
+} \
 ");
-
 });
+
+
