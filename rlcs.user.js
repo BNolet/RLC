bfb1ee7702831198c2dce4a7a7fdd40f4e0da364
Andreas Stjerneklar Kristensen <andreas.kristensen@gmail.com>
2016-04-08 18:44:45 +0200

    Update rlcs.user.js

--- rlcs.user.js
+++ rlcs.user.js
@@ -1,17 +1,29 @@
-
 // ==UserScript==
 // @name         RLCS
 // @namespace    http://tampermonkey.net/
 // @version      0.way too much
 // @description  Parrot-like functionality for Reddit Live
-// @author       FatherDerp, a few of Stjerneklars brain cells
-// @include      https://www.reddit.com/live/wpytzw1guzg2*
+// @author       FatherDerp, Stjerneklar
+// @include      https://www.reddit.com/live/*
 // @require      http://code.jquery.com/jquery-latest.js
+// @grant   GM_addStyle
 // ==/UserScript==                
 $(document).ready(function() {
   $(document).keydown(function(e){
       if (e.keyCode == 13 && e.ctrlKey == true) {
           $(".save-button .btn").click();  
       }
-  });  
+  });   
+ 
+ /*add css styles, every line must end with \  */
+     GM_addStyle(" \
+         a.author {float:left;}   \
+         .liveupdate-listing li.liveupdate .time {width:80px}  \
+         .liveupdate .body {   \
+            background: darkslategrey; \
+            padding: 5px; \
+            max-width:600px!important; \
+         } \
+");
+
 });
