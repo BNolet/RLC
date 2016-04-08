c095fa28b4dc822bc65759bc1e925f9b7e1d550d
BNolet <BNolet@users.noreply.github.com>
2016-04-08 12:27:47 -0400

    Created Userscript
    Feature #1 is to implement Ctrl+Enter to send chat. It's a bodge, but it works.

---
+++ rlcs.user.js
@@ -0,0 +1,17 @@
+
+// ==UserScript==
+// @name         live chatroom robinizer
+// @namespace    http://tampermonkey.net/
+// @version      0.way too much
+// @description  Parrot-like functionality for Reddit Live
+// @author       FatherDerp, a few of Stjerneklars brain cells
+// @include      https://www.reddit.com/live/wpytzw1guzg2*
+// @require      http://code.jquery.com/jquery-latest.js
+// ==/UserScript==                
+$(document).ready(function() {
+  $(document).keydown(function(e){
+      if (e.keyCode == 13 && e.ctrlKey == true) {
+          $(".save-button .btn").click();  
+      }
+  });  
+});
