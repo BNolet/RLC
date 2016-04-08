019664236a5c272a29a70b882d07154143b0226e
BNolet <BNolet@users.noreply.github.com>
2016-04-08 13:41:21 -0400

    Update rlcs.user.js

--- rlcs.user.js
+++ rlcs.user.js
@@ -18,10 +18,16 @@ $(document).ready(function() {
  /*add css styles, every line must end with \  */
      GM_addStyle(" \
           a.author {float:left;} \
-          body.loggedin.liveupdate-app {background:#575757;color:white;}\
-          div.content{background:#575757;color:white;}\
+          aside.sidebar #discussions li {background:#404040;}\
+          .md a {color:#5ED7FF!important;}\
+          .sidebar a {color:#5ED7FF!important;}\
+          body.loggedin.liveupdate-app {background:#404040;color:white;}\
+          div.content{background:#404040;color:white;}\
           div.md{color:white;}\
-          .liveupdate-listing li.liveupdate ul.buttonrow { display: none!important;}\
+          div#liveupdate-options {color:white;}\
+          aside.sidebar {background:#404040;!important}\
+          h2{color:white!important}\
+         .liveupdate-listing li.liveupdate ul.buttonrow { display: none!important;}\
          .liveupdate-listing li.liveupdate .time {width:80px}  \
          .liveupdate .body {   \
             padding: 5px; \
