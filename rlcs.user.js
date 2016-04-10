725d3cbbbf36685967a62cfe469e2ab8e05e3fc7
Andreas Stjerneklar Kristensen <andreas.kristensen@gmail.com>
2016-04-10 12:22:08 +0200

    Update rlcs.user.js

--- rlcs.user.js
+++ rlcs.user.js
@@ -1,15 +1,21 @@
 // ==UserScript==
 // @name         RLCS
 // @namespace    http://tampermonkey.net/
-// @version      0.7 - death to embedly 
-// @description  Parrot-like functionality for Reddit Live
+// @version      0.9 Revamp 
+// @description  Chat-like functionality for Reddit Live
 // @author       FatherDerp, Stjerneklar
-// @include      https://*.reddit.com/live/*
-// @exclude      https://*.reddit.com/live/*/edit
+// @include      https://www.reddit.com/live/*
 // @require      http://code.jquery.com/jquery-latest.js
 // @grant   GM_addStyle
 // ==/UserScript==                
 $(document).ready(function() {
+  
+$("body").append('<div id="rlc-main"><div id="rlc-chat"></div><div id="rlc-messagebox"></div></div><div id="rlc-sidebar"></div>');
+$('.liveupdate-listing').appendTo('#rlc-chat');
+$('#new-update-form').appendTo('#rlc-messagebox');
+$('#liveupdate-header').prependTo('#rlc-sidebar');
+$('#liveupdate-options').prependTo('#rlc-sidebar');
+$('.main-content aside.sidebar').appendTo('#rlc-sidebar');
   
   $(document).keydown(function(e){
       if (e.keyCode == 13) {
@@ -23,93 +29,95 @@ $(document).ready(function() {
         // Focus textarea and set the value of textarea
         $(".usertext-edit.md-container textarea").focus().val("").val(source + " " + username + " ");
     });
- });
- /*add css styles, every line must end with \  */
+ 
+});
+/*add css styles, every line must end with \  */
       GM_addStyle(" \
-        .liveupdate-listing li.liveupdate ul.buttonrow { display: none!important;} \
-        .liveupdate-listing li.liveupdate .time {}   \
-        .liveupdate .body {max-width: none!important;}  \
-        aside.sidebar #discussions li {background:#404040;} \
-        .md a {color:#5ED7FF!important;} \
-        .sidebar a {color:#5ED7FF!important;} \
-        body.loggedin.liveupdate-app {background:#404040;color:white;} \
-        div.content{background:#404040;color:white;} \
-        div.md{color:white;} \
-        div#liveupdate-options {color:white;position: absolute;top: 146px;right: 20px;width:auto;} \
-        aside.sidebar {background:#404040;!important} \
-        h2{color:white!important} \
-        #new-update-form .usertext {  max-width: none;} \
+#rlc-main { \
+    width: 70%; \
+    height: 100%; \
+    position: fixed; \
+    top: 63px; \
+    left:0; \
+} \
+#rlc-sidebar { \
+    width: 30%; \
+    height: 100%; \
+    position: fixed; \
+    top: 63px; \
+    right:0; \
+  display:block; \
+} \
+div#liveupdate-options {color:white;position: absolute;bottom: 60px;left: 5px;} \
+.footer-parent, .separator,\
+.liveupdate-listing li.liveupdate ul.buttonrow,\
+body > .content {display: none!important;}\
+.liveupdate .body {max-width: none!important;margin-bottom: 0!important;padding:2px!important;}  \
+#new-update-form .usertext {max-width: 100%;float: left;width: 92%;} \
 .usertext-edit .md {min-width: 100%!important;} \
-.content { \
-    width: 95%; \
-    position:relative; \
-} \
-header#liveupdate-header { \
-    position: absolute; \
-    right: 10px; \
-    top:10px; \
-    width: 21%; \
-} \
+div#new-update-form textarea { height:25px;overflow:hidden;  } \
 div#new-update-form { \
-    position: absolute; \
-    bottom: 5px; \
-    background: #161616; \
-    width: 74%; \
-    margin: 0; \
-    left: 10px; \
-} \
-.main-content { \
-    margin-top: 0; \
     width: 100%; \
-} \
-aside.sidebar.side.md-container { \
-    position: absolute; \
-    right: 0; \
-    top: 190px; \
-    padding:10px; \
-    width:auto;  \
-    max-width: 21%; \
-} \
-ol.liveupdate-listing { \
-    max-width: 76%; \
-    overflow-y: scroll; \
-    padding-right: 15px; \
-    height: calc(100vh - 250px); \
-    margin-bottom: 150px; \
-} \
-.liveupdate-listing li.liveupdate { border-top: none;margin-top:2px;padding:0px!important;} \
-.liveupdate-listing li.liveupdate { background:darkslategray;color:white;} \
-.liveupdate-listing li.liveupdate .body div.md { \
-    width: 84%; \
-    display: block; \
-    float: right; \
-    margin-bottom:0; \
-    max-width: none; \
-} \
-a.author { \
-    display: block; \
-    width: 15%; \
-    float: left; \
-    } \
-.liveupdate-listing li.liveupdate time { \
-    padding: 0; \
-    width: 15%; \
-    float:left; \
+    margin: 0; \
 } \
 .usertext-edit.md-container { \
     max-width: 100%; \
     margin: 0; \
 } \
+aside.sidebar.side.md-container { \
+    max-width:100%;\
+    width:98%;\
+} \
+ol.liveupdate-listing { \
+    max-width: 100%; \
+    overflow-y: scroll;  \
+    height: calc(100vh - 130px); \
+    padding:5px;\
+    box-sizing:border-box;\
+} \
+.liveupdate-listing li.liveupdate {  border-top:1px solid grey;padding-top:2px; } \
+.liveupdate-listing li.liveupdate .body div.md {\
+    width: 86%;\
+    display: block;\
+    float: right;\
+    margin-bottom:0;\
+    max-width: none;\
+} \
+a.author {\
+    display: block;\
+    width: 12%;\
+    float: left;\
+    margin: 0;\
+    text-align: right;\
+    } \
+.liveupdate-listing li.liveupdate time {\
+    padding: 0;\
+    width: 12%;\
+    float:left;\
+    margin: 0;\
+    text-align: right;\
+} \
+.liveupdate-listing li.liveupdate time:before {\
+ display:none; \
+}  \
 ol.liveupdate-listing { \
     display: flex; \
     flex-direction: column-reverse; \
 } \
-.separator { display:none;} \
 .liveupdate-listing li.liveupdate {height: auto!important;overflow:visible;} \
-.footer-parent {    display: none; } \
-.liveupdate-listing li.liveupdate { position:relative;} \
-.live-timestamp {  position: absolute;    top: 20px;    left: 5px;} \
-.liveupdate-listing li.liveupdate a.author {padding:2px;word-break: normal;overflow: hidden;} \
-body {    overflow-y: hidden;} \
-iframe, .embedFrame {    display: none!important;} \
-                  ");
+#liveupdate-header {\
+    width: 100%;\
+    margin:0!important;\
+    padding:0!important;\
+    text-align:center;\
+    max-width: none;\
+} \
+aside.sidebar #discussions li {background:#404040;} \
+.md a {color:#5ED7FF!important;} \
+.sidebar a {color:#5ED7FF!important;} \
+body.loggedin.liveupdate-app {background:#404040;color:white;} \
+div.content{background:#404040;color:white;} \
+div.md{color:white;} \
+aside.sidebar {background:#404040;!important} \
+h2{color:white!important} \
+");
