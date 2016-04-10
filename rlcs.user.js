d035b64939e078af91e1619508edc1ea6a4f79dd
Andreas Stjerneklar Kristensen <andreas.kristensen@gmail.com>
2016-04-10 14:29:06 +0200

    css restructure and organization, larger messagebox

--- rlcs.user.js
+++ rlcs.user.js
@@ -13,15 +13,13 @@
 // @grant   GM_addStyle
 // ==/UserScript==                
 $(document).ready(function() {
-  
-$("body").append('<div id="rlc-main"><div id="rlc-chat"></div><div id="rlc-messagebox"></div></div><div id="rlc-sidebar"></div>');
-$('.liveupdate-listing').appendTo('#rlc-chat');
-$('#new-update-form').appendTo('#rlc-messagebox');
-$('#liveupdate-header').prependTo('#rlc-sidebar');
-$('#liveupdate-options').prependTo('#rlc-sidebar');
-$('.main-content aside.sidebar').appendTo('#rlc-sidebar');
-  
-  $(document).keydown(function(e){
+    $("body").append('<div id="rlc-main"><div id="rlc-chat"></div><div id="rlc-messagebox"></div></div><div id="rlc-sidebar"></div>');
+    $('.liveupdate-listing').appendTo('#rlc-chat');
+    $('#new-update-form').appendTo('#rlc-messagebox');
+    $('#liveupdate-header').prependTo('#rlc-sidebar');
+    $('#liveupdate-options').prependTo('#rlc-sidebar');
+    $('.main-content aside.sidebar').appendTo('#rlc-sidebar');
+    $(document).keydown(function(e){
       if (e.keyCode == 13) {
           $(".save-button .btn").click();  
       }
@@ -33,10 +31,10 @@ $('.main-content aside.sidebar').appendTo('#rlc-sidebar');
         // Focus textarea and set the value of textarea
         $(".usertext-edit.md-container textarea").focus().val("").val(source + " " + username + " ");
     });
- 
 });
 /*add css styles, every line must end with \  */
       GM_addStyle(" \
+/* Custom elements */\
 #rlc-main { \
     width: 70%; \
     height: 100%; \
@@ -50,43 +48,42 @@ $('.main-content aside.sidebar').appendTo('#rlc-sidebar');
     position: fixed; \
     top: 63px; \
     right:0; \
-  display:block; \
 } \
-#liveupdate-options {position: absolute;top:116px;left: 5px;} \
-.res-nightmode #liveupdate-options {color:white;} \
-.footer-parent, .liveupdate-listing .separator,\
+/*general modifications*/\
+#liveupdate-options {\
+    position: absolute;\
+    top:116px;\
+    left: 5px;\
+} \
+.res-nightmode #liveupdate-options {color:white;}\
+/* hard removal */\
+.footer-parent, \
+.liveupdate-listing .separator,\
 .liveupdate-listing li.liveupdate ul.buttonrow,\
-body > .content {display: none!important;}\
-.liveupdate .body {max-width: none!important;margin-bottom: 0!important;padding:2px!important;}  \
-#new-update-form .usertext {max-width: 100%;float: left;width: 100%;} \
-.usertext-edit .md {min-width: 100%!important;} \
-div#new-update-form textarea { height:25px;overflow:hidden;  } \
-div#new-update-form { \
-    width: 100%; \
-    margin: 0; \
-} \
-.usertext-edit.md-container { \
-    max-width: 100%; \
-    margin: 0; \
-} \
-aside.sidebar.side.md-container { \
-    max-width:100%;\
-    width:98%;\
-} \
-ol.liveupdate-listing { \
+.liveupdate-listing li.liveupdate time:before,\
+.help-toggle, \
+.reddiquette,\
+body > .content { display: none!important; }\
+/*chat window*/\
+.liveupdate-listing { \
     max-width: 100%; \
     overflow-y: scroll;  \
-    height: calc(100vh - 118px); \
+    height: calc(100vh - 138px); \
     padding:5px;\
     box-sizing:border-box;\
+    display: flex; \
+    flex-direction: column-reverse; \
 } \
-.liveupdate-listing li.liveupdate {  border-top:1px solid grey;padding-top:2px; } \
-.liveupdate-listing li.liveupdate .body div.md {\
-    width: 86%;\
-    display: block;\
-    float: right;\
-    margin-bottom:0;\
-    max-width: none;\
+.liveupdate-listing .liveupdate .body {\
+    max-width: none!important;\
+    margin-bottom: 0!important;\
+    padding:2px!important;\
+}  \
+.liveupdate-listing .liveupdate {  \
+    border-top:1px solid grey;\
+    padding-top:2px; \
+    height: auto!important; \
+    overflow:visible!important;\
 } \
 .liveupdate-listing a.author {\
     display: block;\
@@ -95,30 +92,40 @@ ol.liveupdate-listing { \
     margin: 0;\
     text-align: right;\
     } \
-.liveupdate-listing li.liveupdate time {\
+.liveupdate-listing .liveupdate time {\
     padding: 0;\
     width: 12%;\
     float:left;\
     margin: 0;\
     text-align: right;\
 } \
-.liveupdate-listing li.liveupdate time:before {\
- display:none; \
-}  \
-ol.liveupdate-listing { \
-    display: flex; \
-    flex-direction: column-reverse; \
-} \
-.liveupdate-listing li.liveupdate {height: auto!important;overflow:visible;} \
-#liveupdate-header {\
-    width: 100%;\
-    margin:0!important;\
-    padding:0!important;\
-    text-align:center;\
+.liveupdate-listing .liveupdate .body div.md {\
+    width: 86%;\
+    display: block;\
+    float: right;\
+    margin-bottom:0;\
     max-width: none;\
 } \
+/*message input and send button*/\
+#new-update-form .usertext {\
+    max-width: 100%;\
+    float: left;\
+    width: 100%;\
+} \
+.usertext-edit .md {min-width: 100%!important;} \
+div#new-update-form textarea { \
+    height:45px;\
+    overflow:hidden;  \
+} \
+div#new-update-form { \
+    width: 100%; \
+    margin: 0; \
+} \
+.usertext-edit.md-container { \
+    max-width: 100%; \
+    margin: 0; \
+} \
 .usertext-edit.md-container {  position: relative;}  \
-.help-toggle, .reddiquette {display:none!important} \
 #new-update-form .bottom-area { \
     position: absolute; \
     top: 4px; \
@@ -136,5 +143,17 @@ ol.liveupdate-listing { \
 } \
 .usertext-edit.md-container textarea { \
     padding:2px; \
+} \
+/*sidebar*/\
+aside.sidebar.side.md-container { \
+    max-width:100%;\
+    width:98%;\
+} \
+#liveupdate-header {\
+    width: 100%;\
+    margin:0!important;\
+    padding:0!important;\
+    text-align:center;\
+    max-width: none;\
 } \
 ");
