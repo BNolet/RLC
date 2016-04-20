70c7ffbd404c83181f31026fb3dfd990aea8aa09
Andreas Stjerneklar Kristensen <andreas.kristensen@gmail.com>
2016-04-21 00:24:46 +0200

    fix emotes

--- rlcs.user.js
+++ rlcs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RLC
 // @namespace    http://tampermonkey.net/
-// @version      2.7.2
+// @version      2.7.3
 // @description  Chat-like functionality for Reddit Live
 // @author       FatherDerp, Stjerneklar, thybag, mofosyne, jhon, MrSpicyWeiner
 // @include      https://www.reddit.com/live/*
@@ -37,7 +37,7 @@
     var activeUserTimes = [];
     var updateArray = [];
     
-    var emojiList={":)":"smile",":((":"angry",":(":"frown",":s":"silly", ":S":"silly", ":l":"meh",  ":|":"meh", ":/":"meh",":o":"shocked",":O":"shocked"};
+    var emojiList={":)":"smile",":((":"angry",":(":"frown",":s":"silly",":|":"meh",":o":"shocked"};
     
     // Html for injection, inserted at doc.ready
     var htmlPayload = '  \
