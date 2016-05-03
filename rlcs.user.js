377693dc817d98fa6809b9ab1bfb774306329468
BNolet <BNolet@users.noreply.github.com>
2016-05-03 13:47:44 -0400

    ver num fix
    ver number

--- rlcs.user.js
+++ rlcs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RLC
 // @namespace    http://tampermonkey.net/
-// @version      3.7.6
+// @version      3.7.7
 // @description  Chat-like functionality for Reddit Live
 // @author       FatherDerp, Stjerneklar, thybag, mofosyne, jhon, 741456963789852123, MrSpicyWeiner, Concerned Hobbit (TheVarmari)
 // @include      https://www.reddit.com/live/*
