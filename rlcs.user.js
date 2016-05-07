713e9990790475d7abacd59d136547a89918a31a
Andreas Stjerneklar Kristensen <andreas.kristensen@gmail.com>
2016-05-08 00:19:26 +0200

    Update rlcs.user.js

--- rlcs.user.js
+++ rlcs.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           RLC
-// @version        3.12.10
+// @version        3.12.11
 // @description    Chat-like functionality for Reddit Live
 // @author         FatherDerp & Stjerneklar
 // @contributor    FatherDerp, Stjerneklar, thybag, mofosyne, jhon, 741456963789852123, MrSpicyWeiner, Concerned Hobbit (TheVarmari), Kretenkobr2
