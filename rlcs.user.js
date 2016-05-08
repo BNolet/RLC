dbdd8590edf264448feaf3240e4e8d5d29108300
Andreas Stjerneklar Kristensen <andreas.kristensen@gmail.com>
2016-05-08 10:51:13 +0200

    stop realternating background on every message
    ffs, this was lagging things like fuck

--- rlcs.user.js
+++ rlcs.user.js
@@ -1350,7 +1350,7 @@
 
         // Stuff that should not be done to messages loaded on init, like TTS handling
         if (loadingInitialMessages === 0) {
-            reAlternate();
+           // reAlternate();
             if (rescan) {
                 // This is rescan, do nothing.
             }
