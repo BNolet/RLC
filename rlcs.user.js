ff87d6988aea0398db859cba1d61887337ed9ee9
kretenkobr2 <kretenkobr2@hotmail.com>
2016-05-15 14:21:23 +0200

    Twitch emotes can be disabled seperately

--- rlcs.user.js
+++ rlcs.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           RLC
-// @version        3.16.7
+// @version        3.16.8
 // @description    Chat-like functionality for Reddit Live
 // @author         FatherDerp & Stjerneklar
 // @contributor    thybag, mofosyne, jhon, FlamingObsidian, MrSpicyWeiner, TheVarmari, Kretenkobr2, dashed
